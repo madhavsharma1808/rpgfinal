d51f384a2a4e20a4065d07374f1468aa8c83f7a7
madhavsharma1808 <madhav.189302106@muj.manipal.edu>
2020-06-28 11:17:52 +0530

    problem with player rotation on moving

--- rpgfinal/Assets/scripts/PlayerMovement.cs
+++ rpgfinal/Assets/scripts/PlayerMovement.cs
@@ -5,10 +5,12 @@ using UnityStandardAssets.Characters.ThirdPerson;
 [RequireComponent(typeof (ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float minimumoffsetdistance=0.2f;
     ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
     CameraRaycaster cameraRaycaster;
     Vector3 currentClickTarget;
-        
+    public float awaycurserdistance;
+    bool isdirect = false;
     private void Start()
     {
         cameraRaycaster = Camera.main.GetComponent<CameraRaycaster>();
@@ -19,7 +21,39 @@ public class PlayerMovement : MonoBehaviour
     // Fixed update is called in sync with physics
     private void FixedUpdate()
     {
-        if (Input.GetMouseButton(0))
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            isdirect = !isdirect;
+        }    
+        if(isdirect)
+            keyboardmovement();
+        else
+            cursormovement();
+        
+    }
+    private void keyboardmovement()
+
+    {
+        print("direct");
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        bool crouch = Input.GetKey(KeyCode.C);
+
+        // calculate move direction to pass to character
+        if (Camera.main != null)
+        {
+            // calculate camera relative direction to move:
+            Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+            Vector3 m_Move = v * m_CamForward + h * Camera.main.transform.right;
+            m_Character.Move(m_Move, false, false);
+        }
+       
+    }
+
+    private void cursormovement()
+    {
+        print("indirect");
+        if (Input.GetMouseButton(0)) 
         {
             switch (cameraRaycaster.layerHit)
             {
@@ -27,16 +61,25 @@ public class PlayerMovement : MonoBehaviour
                     currentClickTarget = cameraRaycaster.hit.point;
                     break;
                 case Layer.Enemy:
-                    print("enemy");
+                    print("enemy");//will not move for now to enemy
                     break;
                 case Layer.RaycastEndStop:
                     print("unexpected area");
                     break;
             }
-            print("Cursor raycast hit" + cameraRaycaster.layerHit);
-            
+            print( cameraRaycaster.layerHit);
+
         }
-        m_Character.Move(currentClickTarget - transform.position, false, false);
+        //error in player facingdirection
+         Vector3 distancetobecovered = currentClickTarget - transform.position;
+        if (distancetobecovered.magnitude >= minimumoffsetdistance)
+        {
+            m_Character.Move(distancetobecovered, false, false);
+        }
+        else
+        {
+            m_Character.Move(Vector3.zero, false, false);
+        }
     }
 }
 
---
+++ rpgfinal/Assets/scripts/cursortexture.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursortexture : MonoBehaviour
+{
+    [SerializeField] Texture2D walkcursor;
+    [SerializeField] Texture2D enemycursor;
+    [SerializeField] Texture2D unknowncursor;
+    [SerializeField] Vector2 cursorhotspot = new Vector2(96, 96);
+
+    Texture2D currentcursor;
+    CameraRaycaster cameraRaycaster;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(cameraRaycaster.layerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor(walkcursor, cursorhotspot, CursorMode.Auto);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor(enemycursor, cursorhotspot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(unknowncursor, cursorhotspot, CursorMode.Auto);
+                break;
+        }
+        Cursor.SetCursor(walkcursor, cursorhotspot, CursorMode.Auto);
+    }
+       
+}
